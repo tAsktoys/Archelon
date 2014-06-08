315c9e3f23941e5cee5236466122f1294b128225
mikan <yutakakato@icloud.com>
2014-06-09 00:28:32 +0900

    Created annotation driven configuration.

--- Archelon/src/java/com/tasktoys/archelon/controller/HelloController.java
+++ Archelon/src/java/com/tasktoys/archelon/controller/HelloController.java
@@ -6,15 +6,18 @@ package com.tasktoys.archelon.controller;
 import com.tasktoys.archelon.service.HelloService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 /**
  * The Hello Controller.
  *
  * @author mikan
  */
-public class HelloController extends AbstractController {
+@Controller
+public class HelloController {
 
     private HelloService helloService;
     
@@ -26,10 +29,14 @@ public class HelloController extends AbstractController {
         this.helloService = helloService;
     }
 
-    @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ModelAndView mv = new ModelAndView("hello");
         mv.addObject("helloMessage", helloService.sayHello("ほげほげ"));
         return mv;
     }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "hello";
+    } 
 }
