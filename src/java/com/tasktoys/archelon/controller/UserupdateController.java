bf9f5369c6342f76e1bb459adf100ce996691964
YuichiroSato <physics.for.high.school.students@gmail.com>
2014-07-10 06:19:39 +0900

    userupdateページにフォームを出力できるようにした

--- src/java/com/tasktoys/archelon/controller/UserupdateController.java
+++ src/java/com/tasktoys/archelon/controller/UserupdateController.java
@@ -6,6 +6,8 @@
 
 package com.tasktoys.archelon.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,17 +22,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value="/userupdate")
 public class UserupdateController {
     
+    final static private String USERUPDATE = "userupdate";
     final static private String ID = "id";
+    final static private String INPUTFORMLIST = "input_form_list";
+    
+    final static private String ENTITY = "entity";
+    final static private String DATA = "data";
     
     @RequestMapping(method = RequestMethod.GET)
     public String getUserGuest(Model model) {
         model.addAttribute(ID, "Guest");
-        return "userupdate";
+        
+        ArrayList<HashMap<String, String>> list = new ArrayList<>();
+        list.add(makeMap("name", "Kato"));
+        list.add(makeMap("age","16"));
+        list.add(makeMap("school",""));
+        model.addAttribute(INPUTFORMLIST, list);
+        
+        return USERUPDATE;
     }
     
     @RequestMapping(value="{id}", method = RequestMethod.GET)
     public String getUser(@PathVariable String id, Model model) {
         model.addAttribute(ID, id);
-        return "userupdate";
+        
+        ArrayList<HashMap<String, String>> list = new ArrayList<>();
+        list.add(makeMap("name", id));
+        list.add(makeMap("age","16"));
+        list.add(makeMap("school",""));
+        model.addAttribute(INPUTFORMLIST, list);
+        
+        return USERUPDATE;
+    }
+    
+    private HashMap<String, String> makeMap(String entity, String data) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(ENTITY, entity);
+        map.put(DATA, data);
+        return map;
     }
 }
--- web/WEB-INF/jsp/userupdate.jsp
+++ web/WEB-INF/jsp/userupdate.jsp
@@ -3,15 +3,35 @@
     Created on : 2014/07/09, 22:33:34
     Author     : Yuichiro
 --%>
-
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <!-- Style fragment -->
+        <%@include file="/WEB-INF/jspf/style.jspf" %>
+        <title><spring:message code="app.name" /> ${id}</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <!-- Header fragment -->
+        <%@include file="/WEB-INF/jspf/header.jspf" %>
+        
+        <div id="main">
+            <h2><spring:message code="userupdate.title" /></h2>
+            <form>
+                <table>
+                    <c:forEach var="item" items="${input_form_list}">
+                        <tr>
+                            <td><c:out value="${item.entity}"/></td>
+                            <td><input type="text" name="<c:out value="${item.entity}"/>" size="30" value="<c:out value="${item.data}"/>"></td>
+                        </tr>
+                    </c:forEach>
+                </table>
+            </form>
+        </div>
+
+        <!-- Footer fragment -->
+        <%@include file="/WEB-INF/jspf/footer.jspf" %>
     </body>
 </html>
