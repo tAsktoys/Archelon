8651c82d1f9616d905fb26766b99231aa4fedb04
YuichiroSato <physics.for.high.school.students@gmail.com>
2014-08-03 00:35:08 +0900

    Discussion Dao made #2

---
+++ src/java/com/tasktoys/archelon/data/entity/Discussion.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright(C) 2014 tAsktoys Project. All rights reserved.
+ */
+
+package com.tasktoys.archelon.data.entity;
+
+/**
+ *
+ * @author Yuichiro
+ */
+public class Discussion {
+    
+}
