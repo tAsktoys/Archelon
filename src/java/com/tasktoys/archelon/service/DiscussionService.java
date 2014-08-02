1086bc7063bf71324fd90a65b880267156061dbf
YuichiroSato <physics.for.high.school.students@gmail.com>
2014-08-03 02:01:52 +0900

    DiscussionService is made #2

---
+++ src/java/com/tasktoys/archelon/service/DiscussionService.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright(C) 2014 tAsktoys Project. All rights reserved.
+ */
+package com.tasktoys.archelon.service;
+
+import com.tasktoys.archelon.data.entity.Discussion;
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ *
+ * @author YuichiroSato
+ */
+public interface DiscussionService {
+    
+    public List<Discussion> getNewestDiscussionList(int n);
+    public List<Discussion> getDiscussionListBefor(BigInteger id, int n);
+    public List<Discussion> getNewestDiscussionListByMainCategory(int n, int main_id);
+    public List<Discussion> getDiscussionListWithMainCategoryBefor(BigInteger id, int n, int main_id);
+    public List<Discussion> getNewestDiscussionListBySubCategory(int n, int main_id, int sub_id);
+    public List<Discussion> getDiscussionListWithSubCategoryBefor(BigInteger id, int n, int main_id, int sub_id);
+}
