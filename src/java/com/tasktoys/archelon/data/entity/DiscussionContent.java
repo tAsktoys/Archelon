e7cc48d5bec1050d378510285f5ddf9527de0c2a
mikan <yutakakato@icloud.com>
2014-08-10 01:22:37 +0900

    refs #4 Added libraries

---
+++ src/java/com/tasktoys/archelon/data/entity/DiscussionContent.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright(C) 2014 tAsktoys. All rights reserved.
+ */
+package com.tasktoys.archelon.data.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author mikan
+ */
+@Document
+public class DiscussionContent {
+    
+}
---
+++ src/java/com/tasktoys/archelon/data/repository/DiscussionContentRepository.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright(C) 2014 tAsktoys. All rights reserved.
+ */
+package com.tasktoys.archelon.data.repository;
+
+/**
+ *
+ * @author mikan
+ */
+public interface DiscussionContentRepository {
+    
+}
