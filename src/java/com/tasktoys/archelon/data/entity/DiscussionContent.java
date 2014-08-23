f3cc2ceb99492c34d48c80d2e99f05e5814c7ccf
mikan <yutakakato@icloud.com>
2014-08-24 00:23:34 +0900

    Updated entity.

--- src/java/com/tasktoys/archelon/data/entity/DiscussionContent.java
+++ src/java/com/tasktoys/archelon/data/entity/DiscussionContent.java
@@ -3,13 +3,100 @@
  */
 package com.tasktoys.archelon.data.entity;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 /**
+ * 
  *
  * @author mikan
  */
 @Document
 public class DiscussionContent {
+    
+    @Id
+    private String id;
+    
+    @Field
+    private long discussionId;
+    
+    @Field
+    private String subject;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getDiscussionId() {
+        return discussionId;
+    }
+
+    public void setDiscussionId(long discussionId) {
+        this.discussionId = discussionId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    
+    
+}
+
+class Post {
+    
+    private long authorId;
+    private String description;
+    private String meth;
+    private String fig;
+    private String svg;
+
+    public long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMeth() {
+        return meth;
+    }
+
+    public void setMeth(String meth) {
+        this.meth = meth;
+    }
+
+    public String getFig() {
+        return fig;
+    }
+
+    public void setFig(String fig) {
+        this.fig = fig;
+    }
+
+    public String getSvg() {
+        return svg;
+    }
+
+    public void setSvg(String svg) {
+        this.svg = svg;
+    }
     
 }
--- src/java/com/tasktoys/archelon/data/repository/DiscussionContentRepository.java
+++ src/java/com/tasktoys/archelon/data/repository/DiscussionContentRepository.java
@@ -3,10 +3,18 @@
  */
 package com.tasktoys.archelon.data.repository;
 
+import com.tasktoys.archelon.data.entity.DiscussionContent;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 /**
  *
  * @author mikan
  */
-public interface DiscussionContentRepository {
+public interface DiscussionContentRepository extends MongoRepository<DiscussionContent, Long> {
     
+    public DiscussionContent findDiscussionContentById(long id);
+    
+    public void insertContent(DiscussionContent content);
+    
+    public void addPost(DiscussionContent content);
 }
